13930ef214237e7653b7d55e0c7815e092f2a2a5
Stamer, Daniel <daniel.stamer@airberlin.com>
2017-06-14 15:03:22 +0200

    it's a teapot
    Signed-off-by: Stamer, Daniel <daniel.stamer@airberlin.com>

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM scratch
+COPY teapot /
+CMD ["/teapot"]
---
+++ teapot.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"io"
+
+	"github.com/gorilla/mux"
+)
+
+var pot = `
+                       (
+            _           ) )
+         _,(_)._        ((
+    ___,(_______).        )
+  ,'__.   /       \    /\_
+ /,' /  |""|       \  /  /
+| | |   |__|       |,'  /
+ \'.|                  /
+  '. :           :    /
+    '.            :.,'
+      '-.________,-'
+`
+
+func main() {
+	log.Println("Teapot booting..")
+	router := mux.NewRouter().StrictSlash(false)
+	router.PathPrefix("/").HandlerFunc(serve)
+	log.Fatal(http.ListenAndServe("localhost:4000", router))
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusTeapot)
+	io.WriteString(w, pot)
+}
